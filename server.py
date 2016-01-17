ac2766043a35b7e2f6b89afc277d46f55e0cbaa2
genevag <geneva@ualberta.ca>
2016-01-16 19:30:02 -0700

    refactoring + code clean up

--- server.py
+++ server.py
@@ -30,6 +30,11 @@ import os
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+'''
+----- PROBLEM -----
+127.0.0.1:8080/deep   doesn't render the css b/c it gets /deep.css instead of /deep/deep.css
+'''
+
 class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
@@ -41,38 +46,63 @@ class MyWebServer(SocketServer.BaseRequestHandler):
         print 'requestType : ' + requestType
         print 'urlEndpoint : ' + urlEndpoint, '\n'
 
+        # normalize file path : changes "/www/deep/../base.css" to "/www/base.css"
+        print "Input Path : " + os.getcwd() + '/www' + urlEndpoint
         path = os.path.normpath(os.getcwd() +  '/www' + urlEndpoint)
+        print "Normalized Path : " + path + '\n'
 
-        if urlEndpoint != '/' and urlEndpoint != '/deep' and urlEndpoint != '/deep/':
-            print 'Path : ' + path, '\n'
-            # req = open('www' + urlEndpoint, 'r')
 
-            # forbidden - restrict access to www directory only and not higher directories
-            if '..' in urlEndpoint:
-                self.request.send("HTTP/1.1 403\r\n")
-                return
 
-            if os.path.exists(path):
-                page = urllib2.urlopen('file://' + path)
-            else:
-                print 'file path not exists'
-                self.request.send("HTTP/1.1 404\r\n")
-                return
+        # ------------------------------------------- #
+        # ----- Validate directory to restricted access only ----- #
+        # forbidden - restrict access to www directory only and not higher directories
+        if (path != os.getcwd() + '/www') and (os.getcwd() + '/www/' not in path):
+            self.request.send("HTTP/1.1 403\r\n")
+            return
+        # ------------------------------------------- #
 
-            self.request.send('HTTP/1.1 200 OK\r\n')
-            self.request.send(str(page.headers))
-            self.request.send(page.read())
 
-        else:
-            path = os.path.normpath(path + '/index.html')
-            print 'Path : ' + path, '\n'
-            
+
+        # ------------------------------------------- #
+        # ------ Here, access is allowed ------ #
+
+
+        # --- Valid/Existing Path & Valid/Existing File --- #
+        # ----- Validate path & file & send 200 response ----- #
+        if os.path.exists(path) and os.path.isfile(path):
             page = urllib2.urlopen('file://' + path)
             self.request.send('HTTP/1.1 200 OK\r\n')
-            self.request.send(str(page.headers))
+            self.request.send(str(page.headers) + '\r\n\r\n')
             self.request.send(page.read())
+        # ------------------------------------------- #
 
-        print '\n --------------------------------', '\n'
+
+
+        # --- In the allowed "/www" directory & path is a directory, not a file --- #
+        # ----- path is '/' or '/deep' --> redirect to index.html file for that directory ----- #
+        elif os.path.exists(path) and os.path.isdir(path):
+            newUrlEndpoint = os.path.normpath(urlEndpoint + '/index.html')
+            newPath = os.path.normpath(os.getcwd() +  '/www/' + newUrlEndpoint)
+
+            print "newUrlEndpoint : " + newUrlEndpoint
+            print "newPath : " + newPath
+
+            # self.request.send('HTTP/1.1 301 Moved Permanently\r\n')
+            # self.request.send('Location: ' + newUrlEndpoint)
+
+            page = urllib2.urlopen('file://' + newPath)
+            self.request.send('HTTP/1.1 200 OK\r\n')
+            self.request.send(str(page.headers) + '\r\n\r\n')
+            self.request.send(page.read())
+        # ------------------------------------------- #
+
+
+
+        # ----- Bad path or non-existing file, send 404 response ---- #
+        else:
+            self.request.send("HTTP/1.1 404\r\n")
+        # ------------------------------------------- #
+
 
 
 
